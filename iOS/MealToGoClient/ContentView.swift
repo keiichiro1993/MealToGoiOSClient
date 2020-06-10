2bd06c0a6db291d87f3c1a35dac787cac03c1e61
Keiichiro Wakasa <keiichiro1993@gmail.com>
2020-06-10 19:07:16 +0900

    restructured to use ContentView

--- iOS/MealToGoClient/ContentView.swift
+++ iOS/MealToGoClient/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showSideMenu = false
+    
+    init(){
+        //NavigationBar のカスタマイズ
+        UITableView.appearance().backgroundColor = .none//UIColor(named: "backgroundLight")
+        UINavigationBar.appearance().barTintColor = UIColor(hex: "ed6a5a")
+        UINavigationBar.appearance().backgroundColor = .none
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor.white, .font : UIFont(name: "HiraMaruProN-W4", size: 20)!]
+        UINavigationBar.appearance().tintColor = .white
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            NavigationView {
+                ZStack(alignment: .leading) {
+                    RestaurantListPage()
+                        .offset(x: self.showSideMenu ? geometry.size.width/2 : 0)
+                        .disabled(self.showSideMenu)
+                    if self.showSideMenu {
+                        SideMenu()
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .navigationBarItems(leading: (
+                    Button(action: {
+                        withAnimation {
+                            self.showSideMenu.toggle()
+                        }
+                    }) {
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                    }
+                ))
+            }
+        }
     }
 }
 
